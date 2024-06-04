6365d55ae5620fee188ad36c2099dde49574e72a
kwhproject <157696203+kwhproject@users.noreply.github.com>
2024-06-04 20:51:54 +0900

    [Bronze III] Title: 곱셈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main()
+{
+	int a;
+	int b;
+	
+	std::cin >> a;
+	std::cin >> b;
+	
+	std::cout << a * ((b % 100) % 10) << std::endl;
+	std::cout << a * ((b % 100) / 10) << std::endl;
+	std::cout << a * (b / 100) << std::endl;
+	std::cout << a * b << std::endl;
+
+	return 0;
+}
