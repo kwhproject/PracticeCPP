5be2748f4c4cbb47cb95716eec0ccf473ab545d0
kwhproject <157696203+kwhproject@users.noreply.github.com>
2024-06-04 20:22:45 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main()
+{
+	int a;
+	int b;
+
+	std::cin >> a;
+	std::cin >> b;
+
+	std::cout << a+b << std::endl;
+	std::cout << a-b << std::endl;
+	std::cout << a*b << std::endl;
+	std::cout << a/b << std::endl;
+	std::cout << a%b << std::endl;
+
+	return 0;
+}
