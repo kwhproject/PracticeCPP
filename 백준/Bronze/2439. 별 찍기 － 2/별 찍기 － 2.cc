30c3cb393de3b0cbf725fb08f1ec06f646f2fbcd
kwhproject <157696203+kwhproject@users.noreply.github.com>
2024-06-06 14:31:24 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main()
+{
+	int n;
+
+	std::cin >> n;
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = n - i - 1; j > 0; j--)
+		{
+			std::cout << " ";
+		}
+		for (int k = 0; k < i + 1; k++)
+		{
+			std::cout << "*";
+		}
+
+		std::cout << std::endl;	
+	}
+
+	return 0;
+}
