7bbdcfd6aff472ed023424cb78448407b1d9885d
kwhproject <157696203+kwhproject@users.noreply.github.com>
2024-06-06 12:57:29 +0900

    [Bronze V] Title: A+B - 7, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main()
+{
+	int n, a, b;
+
+	std::cin >> n;
+
+	for (int i = 0; i < n; i++)
+	{
+		std::cin >> a;
+		std::cin >> b;
+		std::cout << "Case #" << i + 1 << ": " << a + b << std::endl;
+	}
+
+	return 0;
+}
