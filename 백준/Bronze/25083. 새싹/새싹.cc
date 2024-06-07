28c4cd15ea2e2ba02b047ae0b4f847456cb87f48
kwhproject <157696203+kwhproject@users.noreply.github.com>
2024-06-08 02:07:39 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+	std::string s1 = "         ,r'\"7";
+	std::string s2 = "r`-_   ,'  ,/";
+	std::string s3 = " \\. \". L_r'";
+	std::string s4 = "   `~\\/";
+	std::string s5 = "      |";
+	std::string s6 = "      |";
+	std::cout << s1 << std::endl;
+	std::cout << s2 << std::endl;
+	std::cout << s3 << std::endl;
+	std::cout << s4 << std::endl;
+	std::cout << s5 << std::endl;
+	std::cout << s6;
+
+	return 0;
+}
